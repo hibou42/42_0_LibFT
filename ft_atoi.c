16046a9117735ce9b4106bf9cf0003f8728aa3ab
Arnaud Schaefer <aschaefe@student.42lausanne.ch>
2022-10-27 16:26:30 +0200

    .

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aschaefe <aschaefe@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 11:59:11 by aschaefe          #+#    #+#             */
-/*   Updated: 2022/10/27 16:25:40 by aschaefe         ###   ########.fr       */
+/*   Updated: 2022/10/27 16:26:17 by aschaefe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,3 @@ int	ft_atoi(const char *str)
 	}
 	return (res * neg);
 }
-
